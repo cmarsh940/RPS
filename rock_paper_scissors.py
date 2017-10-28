03ff454af29e5ee18688fd5510652c30d42453d6
cam <cam.marshall940@yahoo.com>
2017-10-27 21:21:02 -0500

    rock papper scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import random 
+
+app = Flask(__name__)
+
+def get_computer_move():
+    options = ["rock", "paper", "scissors"]
+    return options[random.randint(0,2)]
+
+def get_winner(player_choice, computer_choice):
+    winner = "computer"
+
+    if player_choice == computer_choice:
+        winner = "tie"
+    if player_choice == "rock" and computer_choice == "scissors":
+        winner = "player"
+    if player_choice == "scissors" and computer_choice == "paper":
+        winner = "player"
+    if player_choice == "paper" and computer_choice == "rock":
+        winner = "player"
+
+    return winner
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/layout/<choice>')
+def result(choice):
+    player_choice = choice.lower()    
+    computer_choice = get_computer_move()
+    winner = get_winner(player_choice, computer_choice)
+    
+    return render_template("result.html", winner=winner, player_choice=player_choice, computer_choice=computer_choice)
+
+app.run(debug=True)
